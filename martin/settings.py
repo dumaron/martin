0bb3eb70f6b1665e824aa93e867e40fb0402e633
Duma <dumaron@gmail.com>
2024-07-25 08:52:34 +0200

    specific css style for finances; tests with css tabs

--- martin/settings.py
+++ martin/settings.py
@@ -110,7 +110,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static",]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    BASE_DIR / "ynab_reconcile_helper/static"
+]
 
 
 # Default primary key field type
