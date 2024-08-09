789d2da7f8ba64b249688430f48ea3d21304091a
Duma <dumaron@gmail.com>
2024-08-09 17:30:54 +0200

    improved basic scaffold for tasks app; attempt at using different header colors (but not happy with it)

--- martin/urls.py
+++ martin/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("finances/", include("ynab_reconcile_helper.urls")),
+    path("tasks/", include("tasks.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
