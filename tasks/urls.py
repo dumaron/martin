3cea0f16d63de82cde9b0e487c05c01a32d6e2e5
Duma <dumaron@gmail.com>
2024-10-18 19:23:06 +0200

    improvements in navigation

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,6 @@ from .views import first_page, project_detail
 
 
 urlpatterns = [
-    path('projects/', first_page, name="tasks_first_page"),
+    path('projects/', first_page, name='tasks_first_page'),
     path('projects/<int:project_id>/', project_detail, name='project_detail'),
 ]
