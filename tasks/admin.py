481b2374836cdc2666f333ae83a2336bac37ca1a
Duma <dumaron@gmail.com>
2024-10-18 19:10:48 +0200

    Extended tasks app models

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Project, Todo
+from .models import Project, Todo, Inbox, Update
 
 admin.site.register(Project)
 admin.site.register(Todo)
+admin.site.register(Inbox)
+admin.site.register(Update)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,5 @@
+from random import choices
+
 from django.db import models
 
 
@@ -31,6 +33,35 @@ class Todo(models.Model):
     project = models.ForeignKey('Project', on_delete=models.CASCADE, blank=True, null=True)
     status = models.CharField(choices=Statuses, max_length=32)
     valid_from = models.DateTimeField(null=True, blank=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    priority = models.SmallIntegerField(default=0)
+    daily_priority_increase = models.SmallIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_valid_order_pray =  models.BooleanField(default=False)
 
     def __str__(self):
         return self.description
+
+
+class Inbox(models.Model):
+
+    class Contexts(models.TextChoices):
+        PRIVATE = 'private'
+        WORK = 'work'
+
+    id = models.AutoField(primary_key=True)
+    content = models.TextField()
+    context = models.TextField(choices=Contexts, default=Contexts.PRIVATE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    processed_at = models.DateTimeField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+
+class Update(models.Model):
+    id = models.AutoField(primary_key=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Update for project {self.project} on {self.created_at}'
