75a634479babd3965526a4649e1d360de1fa978a
Duma <dumaron@gmail.com>
2024-11-09 19:45:39 +0100

    improvements around tasks and flows

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import first_page, project_detail
+from .views import *
 
 
 urlpatterns = [
+    # Project-based views
     path('projects/', first_page, name='tasks_first_page'),
     path('projects/<int:project_id>/', project_detail, name='project_detail'),
+
+    # Flows
+    path('flows/', flows_list, name='flows_list'),
+    path('flows/max-priority', process_tasks_by_priority_flow, name='process_tasks_by_priority_flow'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_GET
 from .models import Project, Todo
+from django.db.models import Max
+
 
 
 @login_required
+@require_GET
 def first_page(request):
+    """
+    TODO
+    """
     top_level_projects = Project.objects.filter(tn_parent__isnull=True, status=Project.Statuses.ACTIVE)
     return render(request, 'first_page.html', { 'projects': top_level_projects })
 
@@ -13,6 +19,9 @@ def first_page(request):
 @login_required
 @require_GET
 def project_detail(request, project_id):
+    """
+    TODO
+    """
     project = get_object_or_404(Project, pk=project_id)
     children_projects = Project.objects.filter(tn_parent=project, status=Project.Statuses.ACTIVE)
     related_todos = Todo.objects.filter(project=project).order_by('description')
@@ -21,3 +30,20 @@ def project_detail(request, project_id):
         'todos': related_todos,
         'children_projects': children_projects,
     })
+
+
+def flows_list(request):
+    """
+    TODO
+    """
+    return render(request, 'flows_list.html', {})
+
+
+def process_tasks_by_priority_flow(request):
+    """
+    TODO
+    """
+
+    max_priority_todo = Todo.objects.filter(status=Todo.Statuses.TODO).order_by('-priority').first()
+
+    return render(request, 'flows/process_tasks_by_priority.html', { 'todo': max_priority_todo })
