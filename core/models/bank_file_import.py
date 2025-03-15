e5df2333fe56275d2aafc1f5f4ff82feeccbf8be
Duma <dumaron@gmail.com>
2025-03-15 14:42:52 +0100

    dropped duplicate files

--- core/models/bank_file_import.py
+++ core/models/bank_file_import.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from finances.adapters.file_reader import get_file_rows
+from core.adapters.file_reader import get_file_rows
 
 User = get_user_model()
 
