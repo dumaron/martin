82d146996740441340b0502adac9b2e6fd906822
Duma <dumaron@gmail.com>
2025-10-04 17:22:09 +0200

    fixed script to fetch database

--- scripts/clone_prod_db.sh
+++ scripts/clone_prod_db.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 mv /Users/duma/dev/martin/db.sqlite3 /Users/duma/dev/martin/db.sqlite3.bk
 fly sftp get /storage/db/db.sqlite3 /Users/duma/dev/martin/db.sqlite3
+fly sftp get /storage/db/db.sqlite3-wal /Users/duma/dev/martin/db.sqlite3-wal
+fly sftp get /storage/db/db.sqlite3-shm /Users/duma/dev/martin/db.sqlite3-shm
 sqlite3 /Users/duma/dev/martin/db.sqlite3 'PRAGMA journal_mode=DELETE;'
 rm /Users/duma/dev/martin/db.sqlite3-shm
