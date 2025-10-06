41d4e29023514f51a32e896962552c153dc99dba
Duma <dumaron@gmail.com>
2025-10-06 21:50:44 +0200

    fixed new system for linking duplicate transactions

--- apps/website/views/pair_transactions_page.py
+++ apps/website/views/pair_transactions_page.py
@@ -119,38 +119,32 @@ def link_duplicate_bank_transaction(request):
 	The transaction ID from the URL becomes a duplicate of the transaction from the query parameter.
 	"""
 
-
-	# TODO now the duplicate_transaction_id field needs to come from the form!!
-
 	# Get the original transaction ID from the query parameter
-	original_transaction_id_str = request.GET.get('bank_transaction')
-
-	# Validate that the bank_transaction parameter was provided
-	if not original_transaction_id_str:
-		return HttpResponseBadRequest('bank_transaction parameter is required')
-
-	original_transaction_id = int(original_transaction_id_str)
+	target_bank_transaction_id = int(request.POST.get('bank_transaction_id'))
+	duplicate_transaction_id = int(request.POST.get('duplicate_transaction_id'))
 
 	# Get both transactions
 	duplicate_transaction = get_object_or_404(BankTransaction, pk=duplicate_transaction_id)
-	original_transaction = get_object_or_404(BankTransaction, pk=original_transaction_id)
+	target_transaction = get_object_or_404(BankTransaction, pk=target_bank_transaction_id)
+
+	# TODO make the rest of the function a class method maybe?
 
 	# Prevent a transaction from being set as a duplicate of itself
-	if duplicate_transaction_id == original_transaction_id:
+	if duplicate_transaction_id == target_bank_transaction_id:
 		return HttpResponseBadRequest('A transaction cannot be set as a duplicate of itself')
 
 	# Prevent a transaction that is already a duplicate from being set as a duplicate again
-	if duplicate_transaction.duplicate_of is not None:
+	if target_transaction.duplicate_of is not None:
 		return HttpResponseBadRequest('Transaction is already marked as a duplicate of another transaction')
 
 	# Make the URL transaction a duplicate of the query parameter transaction
 	# THOUGHT Should this be a BankTransaction method? And maybe all the validation too should be there?
-	duplicate_transaction.duplicate_of = original_transaction
-	duplicate_transaction.save()
+	target_transaction.duplicate_of = duplicate_transaction
+	target_transaction.save()
 
 	# Redirect back to the original transaction detail page or wherever specified
 	# TODO given the new pages structure, think if this still makes sense
-	redirect_to = request.GET.get('redirect-to', f'/finances/bank_transactions/{original_transaction_id}')
+	redirect_to = request.GET.get('redirect-to', f'/finances/bank_transactions/{target_bank_transaction_id}')
 	return redirect(redirect_to)
 
 
