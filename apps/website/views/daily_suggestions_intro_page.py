f38c34665878b95c6aa1352e45dfe70cca520719
Duma <dumaron@gmail.com>
2025-10-22 21:39:49 +0200

    ruff format

--- apps/website/views/daily_suggestions_intro_page.py
+++ apps/website/views/daily_suggestions_intro_page.py
@@ -8,7 +8,6 @@ from django.views.decorators.http import require_GET
 @login_required
 @require_GET
 def daily_suggestions_intro_page(request):
-
 	today = datetime.date.today().strftime('%Y-%m-%d')
 	tomorrow = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
 
