cd584ea57b83b5a7fbf9975e89c18e259441225c
Duma <dumaron@gmail.com>
2025-12-30 20:56:36 +0100

    removed `file_name` reference in tests

--- apps/website/tests/views/test_pairing_view.py
+++ apps/website/tests/views/test_pairing_view.py
@@ -30,7 +30,6 @@ class PairingViewTest(TestCase):
 		# Mock the file processing to avoid actual file operations during testing
 		with patch.object(BankFileImport, 'get_file_rows', return_value=[]):
 			self.file_import = BankFileImport.objects.create(
-				file_name='test_import.csv',
 				file_type=BankFileImport.FileType.UNICREDIT_BANK_ACCOUNT_CSV_EXPORT,
 				bank_file=mock_file,
 			)
