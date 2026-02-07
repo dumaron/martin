6262fd0150758bb5c98ca1a7dc11361e00989b1e
Duma <dumaron@gmail.com>
2026-02-07 15:54:39 +0100

    Remove unused Failure class

--- core/models/project.py
+++ core/models/project.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from utils.failures import Failure
-
 
 class Project(models.Model):
 	STATUS_CHOICES = [
--- utils/failures.py
+++
@@ -1,3 +0,0 @@
-class Failure:
-	def __init__(self, reason: str):
-		self.reason = reason
