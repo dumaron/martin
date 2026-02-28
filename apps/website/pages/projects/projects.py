0a67a576db41fe63d4a1cc99eb7b562673176ed2
Duma <dumaron@gmail.com>
2026-02-28 15:51:57 +0100

    quick bugfix: show right tasks in project work tree

--- apps/website/pages/projects/projects.py
+++ apps/website/pages/projects/projects.py
@@ -9,8 +9,8 @@ page = Page(name='projects_page', base_route='pages/projects')
 
 @page.main
 def main_render(request):
-	root_projects = Project.objects.filter(parent__isnull=True, status='active')
-	orphan_tasks = Task.objects.filter(project__isnull=True, status='pending')
+	root_projects = Project.objects.filter(parent__isnull=True, status=['pending', 'active'])
+	orphan_tasks = Task.objects.filter(project__isnull=True, status=['pending', 'active'])
 
 	return render(
 		request, 'projects/projects.html', {'root_projects': root_projects, 'orphan_tasks': orphan_tasks}
